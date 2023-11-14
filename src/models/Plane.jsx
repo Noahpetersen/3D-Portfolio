12830b43ae5f0200534a47d7837af9e07b9a25ea
Noah Petersen <noahpetersen68@outlook.de>
2023-11-14 21:53:47 +0100

    Plane animation

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import planeScene from '../assets/3d/plane.glb'
-import { useGLTF } from '@react-three/drei'
+import { useAnimations, useGLTF } from '@react-three/drei'
 
 const Plane = ({ isRotating, ...props }) => {
+    const plane = useRef()
     const { scene, animations } = useGLTF(planeScene)
+    const { actions } = useAnimations(animations, plane)
+
+    useEffect(() => {
+        actions['Take 001'].play()
+      
+    }, [isRotating, actions])
+
 
   return (
-    <mesh {...props} >
+    <mesh {...props} ref={plane}>
       <primitive object={scene} />
     </mesh>
   )
