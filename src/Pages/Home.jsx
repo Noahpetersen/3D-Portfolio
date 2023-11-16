795f95bc3ecedb79ff2892bb05cfb21f10fc5e52
Noah Petersen <noahpetersen68@outlook.de>
2023-11-16 23:08:54 +0100

    Backgroundsound for immersiveness

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Experience from '../components/Experience'
 import HomeInfo from '../components/HomeInfo'
 
+import sakura from '../assets/sakura.mp3'
+import { a } from '@react-spring/three'
+import { is } from '@react-spring/shared'
+import { soundoff, soundon } from '../assets/icons'
+
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura))
+  audioRef.current.volume = 0.2
+  audioRef.current.loop = true
   const [currentStage, setCurrentStage] = useState()
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false)
+
+  useEffect(() => {
+    if(isPlayingMusic) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+    }
+  } , [isPlayingMusic])
 
   return (
     <section className='w-full h-screen relative'>
@@ -11,6 +28,14 @@ const Home = () => {
         {currentStage && <HomeInfo currentStage={currentStage}/>}
       </div>
       <Experience currentStage={currentStage} setCurrentStage={setCurrentStage} />
+      <div className='absolute bottom-2 left-2 '>
+        <img 
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className='w-10 h-10 cursor-pointer object-contain'
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        />
+      </div>
     </section>
   )
 }
